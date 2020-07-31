2d184a8a16f0a4900364294c383a92fd03fe075b
Per1vai <felipelinchiu321@gmail.com>
2020-07-31 16:16:08 -0600

    Termine el ejercicio 3

--- P3Lab2_FelipeLin.cpp
+++ P3Lab2_FelipeLin.cpp
@@ -3,7 +3,7 @@
 #include <iomanip>
 #include <math.h>
 using namespace std;
-
+void Ejercicio3_Recurisva(int, double);
 int main()
 {
     int menu;
@@ -15,6 +15,12 @@ int main()
         {
         case 1:
         {
+
+            break;
+        }
+        case 2:
+        {
+
             int n;
             cout << "Ingrese el tamaño de la matriz: ";
             cin >> n;
@@ -26,44 +32,40 @@ int main()
                 {
                     matriz[i][j] = rand() % 89 + 10;
                 }
-                
             }
-            
+
             for (int i = 0; i < n; i++)
             {
                 cout << "[";
                 for (int j = 0; j < n; j++)
                 {
-                    cout << setw(3) <<  matriz[i][j] << " ";
+                    cout << setw(3) << matriz[i][j] << " ";
                 }
                 cout << "]" << endl;
             }
 
-            double mediana=0,suma=0,sumatoria_desviacion=0,desviacion=0;
+            double mediana = 0, suma = 0, sumatoria_desviacion = 0, desviacion = 0;
 
-             for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
                     suma = suma + matriz[i][j];
-                
                 }
             }
-            mediana = suma/n;
+            mediana = suma / n;
             cout << suma << "= suma\n";
             cout << "Mediana = " << mediana << endl;
 
-
-             for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    sumatoria_desviacion += pow(matriz[i][j] - mediana,2);
-                
+                    sumatoria_desviacion += pow(matriz[i][j] - mediana, 2);
                 }
             }
 
-            desviacion = sqrt(sumatoria_desviacion/n);
+            desviacion = sqrt(sumatoria_desviacion / n);
 
             cout << "Sumatoria de la desvicaion estandar= " << sumatoria_desviacion << endl;
             cout << "Desviacion estandar= " << desviacion << endl;
@@ -74,32 +76,29 @@ int main()
             {
                 for (int j = 0; j < n; j++)
                 {
-                    matriz_estandarizada[i][j] = (matriz[i][j]-mediana)/desviacion;
-                
+                    matriz_estandarizada[i][j] = (matriz[i][j] - mediana) / desviacion;
                 }
             }
 
-             for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 cout << "[";
                 for (int j = 0; j < n; j++)
                 {
-                    cout << setw(10) <<  matriz_estandarizada[i][j] << " ";
+                    cout << setw(10) << matriz_estandarizada[i][j] << " ";
                 }
                 cout << "]" << endl;
             }
-
-
-            break;
-        }
-        case 2:
-        {
-
 
             break;
         }
         case 3:
         {
+            int n;
+            cout << "Ingrese el limite: ";
+            cin >> n;
+
+            Ejercicio3_Recurisva(n, 0);
 
             break;
         }
@@ -114,3 +113,19 @@ int main()
     }
 }
 
+void Ejercicio3_Recurisva(int n, double suma)
+{
+    double pi, sumatoria = 0, suma2;
+
+    if (n == 0)
+    {
+        suma += pow(-1, n) / ((2 * n) + 1);
+        pi = 4 * suma;
+        cout << "La aproximacion es = " << pi << endl;
+    }
+    else
+    {
+        suma2 = suma2 + pow(-1, n) / ((2 * n) + 1);
+        Ejercicio3_Recurisva(n - 1, suma + suma2);
+    }
+}
